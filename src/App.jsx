875c0aee73281d5de9e6bd9991496a544544a601
Niharika <nihasrinu.k@gmail.com>
2025-10-17 23:57:47 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project-FOR EXAM PRACTICE</h1>
+      <p>CI/CD and Cloud DevOps</p>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+      <p>BHARGAVI</p>
+    </div>
+  );
+}
+
+export default App;
